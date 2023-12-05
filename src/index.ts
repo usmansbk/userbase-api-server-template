2b52911f3167f98e8384116b1ee89dcd7ac8e3e0
usmansbk <usmansbk@gmail.com>
2023-12-05 01:19:01 +0100

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
 import "@/config/env";
+import express from "express";
+import PinoHttp from "pino-http";
+import logger from "./utils/logger";
+
+const port = 4000;
+const app = express();
+
+app.use(
+  PinoHttp({
+    logger,
+  }),
+);
+
+app.get("/healthz", (req, res) => res.json({ success: true }));
+
+app.get("/generate_204", (req, res) => res.status(204).json({ success: true }));
+
+app.listen(port, () => {
+  logger.info(`listening on port ${port}`);
+});
 
 export default async function main(): Promise<string> {
   return "Hello World!";
