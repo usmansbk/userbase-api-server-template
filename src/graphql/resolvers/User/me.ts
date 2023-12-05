8c41a1c74cff71d23fda8c145f41bea42d5ace35
usmansbk <usmansbk@gmail.com>
2023-12-05 12:43:09 +0100

    add resolver

---
+++ src/graphql/resolvers/User/me.ts
@@ -0,0 +1,11 @@
+import type { AppContext } from "types";
+
+export default {
+  Query: {
+    me(_parent: unknown, _args: never, context: AppContext) {
+      const { currentUser } = context;
+
+      return currentUser;
+    },
+  },
+};
