534796ccf0b5157a1e5d4750185e401fa9832cce
usmansbk <usmansbk@gmail.com>
2023-12-05 14:44:32 +0100

    add permisson field resolvers

--- src/graphql/resolvers/User/Permission/Permission.ts
+++ src/graphql/resolvers/User/Permission/Permission.ts
@@ -1,3 +1,48 @@
+import type { Permission } from "@prisma/client";
+import type { AppContext } from "types";
+
 export default {
-  Permission: {},
+  Permission: {
+    async creator(permission: Permission, _args: never, context: AppContext) {
+      const { prismaClient } = context;
+
+      return await prismaClient.permission
+        .findUnique({
+          where: {
+            id: permission.id,
+          },
+        })
+        .creator();
+    },
+    async userPermissions(
+      permission: Permission,
+      _args: never,
+      context: AppContext,
+    ) {
+      const { prismaClient } = context;
+
+      return await prismaClient.permission
+        .findUnique({
+          where: {
+            id: permission.id,
+          },
+        })
+        .userPermissions();
+    },
+    async rolePermissions(
+      permission: Permission,
+      _args: never,
+      context: AppContext,
+    ) {
+      const { prismaClient } = context;
+
+      return await prismaClient.permission
+        .findUnique({
+          where: {
+            id: permission.id,
+          },
+        })
+        .rolePermissions();
+    },
+  },
 };
