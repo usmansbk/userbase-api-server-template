03706b5fa8220b220ab09ceb8272535a920cc439
usmansbk <usmansbk@gmail.com>
2023-12-06 12:23:29 +0100

    add rbac checks to auth middleware

--- src/v1/middlewares/auth.ts
+++ src/v1/middlewares/auth.ts
@@ -1,8 +1,15 @@
 import AuthenticationError from "@/utils/errors/AuthenticationError";
+import ForbiddenError from "@/utils/errors/ForbiddenError";
 import type { NextFunction, Request, Response } from "express";
 
+interface AuthRule {
+  allow: "roles" | "permissions";
+  roles: string[];
+  permissions: string[];
+}
+
 const authMiddleware =
-  () => (req: Request, res: Response, next: NextFunction) => {
+  (rules?: AuthRule[]) => (req: Request, res: Response, next: NextFunction) => {
     const {
       context: { currentUser, t },
     } = req;
@@ -16,6 +23,31 @@ const authMiddleware =
         ),
       );
     } else {
+      if (rules) {
+        const checks = rules.map(({ allow, roles, permissions }) => {
+          switch (allow) {
+            case "roles": {
+              return roles.some((role) => currentUser.roles.includes(role));
+            }
+            case "permissions": {
+              return permissions.some((permission) =>
+                currentUser.permissions.includes(permission),
+              );
+            }
+            default: {
+              return false;
+            }
+          }
+        });
+
+        if (!checks.some((allowed) => allowed)) {
+          throw new ForbiddenError(
+            t("UNAUTHORIZED", {
+              ns: "error",
+            }),
+          );
+        }
+      }
       next();
     }
   };
