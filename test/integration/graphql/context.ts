7def5330ef2485dbaad0be17ffa3865bbe8641c6
usmansbk <usmansbk@gmail.com>
2023-12-07 00:06:38 +0100

    nit

--- test/integration/graphql/context.ts
+++ test/integration/graphql/context.ts
@@ -2,9 +2,11 @@ import redisClient, { pubsub } from "@/config/redis";
 import logger from "@/utils/logger";
 import i18n from "@/config/i18n";
 import getPrismaClient from "@/config/database";
-import type { AppContext } from "types";
+import type { AppContext, CurrentUser } from "types";
 
-export default async function createMockContext(): Promise<AppContext> {
+export default async function createMockContext(
+  currentUser?: CurrentUser,
+): Promise<AppContext> {
   const i18next = await i18n();
   const prismaClient = getPrismaClient();
 
@@ -13,9 +15,9 @@ export default async function createMockContext(): Promise<AppContext> {
     log: logger,
     t: i18next.t,
     pubsub,
+    currentUser,
     redisClient,
     prismaClient,
-    currentUser: null,
     storage: {
       getSignedDownloadUrl: jest.fn().mockResolvedValue("https://test.com"),
     },
