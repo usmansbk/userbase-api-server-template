526ab2442293ba70a3c977b21a11e4872264ce54
usmansbk <usmansbk@gmail.com>
2023-12-07 00:39:53 +0100

    add snapshot

--- test/integration/graphql/resolvers/User/Mutation/joinWaitlist.spec.ts
+++ test/integration/graphql/resolvers/User/Mutation/joinWaitlist.spec.ts
@@ -1,15 +1,17 @@
 import { createMockApolloServer } from "test/integration/graphql";
 import createMockContext from "test/integration/graphql/context";
 
+const query = `mutation {
+			joinWaitlist(email: "usmansbk@gmail.com") {
+				success
+				message
+			}
+		}`;
+
 describe("Mutation.joinWaitlist", () => {
   it("should add user to the waitlist", async () => {
     const server = createMockApolloServer();
     const contextValue = await createMockContext();
-    const query = `mutation {
-			joinWaitlist(email: "usmansbk@gmail.com") {
-				message
-			}
-		}`;
 
     const response = await server.executeOperation(
       {
---
+++ test/integration/graphql/resolvers/User/Mutation/leaveWaitlist.spec.ts
@@ -0,0 +1,60 @@
+import { createMockApolloServer } from "test/integration/graphql";
+import createMockContext from "test/integration/graphql/context";
+
+const query = `mutation LeaveWaitlist($email: EmailAddress!) {
+			leaveWaitlist(email: $email) {
+        success
+				message
+			}
+		}`;
+
+describe("Mutation.leaveWaitlist", () => {
+  it("should remove user from the waitlist", async () => {
+    const server = createMockApolloServer();
+    const contextValue = await createMockContext();
+
+    const user = await contextValue.prismaClient.user.create({
+      data: {
+        email: "test@email.com",
+        firstName: "Testing",
+        password: "test",
+      },
+    });
+
+    const response = await server.executeOperation(
+      {
+        query,
+        variables: {
+          email: user.email,
+        },
+      },
+      { contextValue },
+    );
+
+    const removed = await contextValue.prismaClient.user.findUnique({
+      where: {
+        id: user.id,
+      },
+    });
+
+    expect(removed).toBeNull();
+    expect(response).toMatchSnapshot();
+  });
+
+  it("should be idempotent", async () => {
+    const server = createMockApolloServer();
+    const contextValue = await createMockContext();
+
+    const response = await server.executeOperation(
+      {
+        query,
+        variables: {
+          email: "test2@email.com",
+        },
+      },
+      { contextValue },
+    );
+
+    expect(response).toMatchSnapshot();
+  });
+});
