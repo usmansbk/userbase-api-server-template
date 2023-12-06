c9961043181fb7d2c105a0ba131ca87fd5a0fc76
usmansbk <usmansbk@gmail.com>
2023-12-06 12:29:17 +0100

    nit

--- src/v1/middlewares/auth.ts
+++ src/v1/middlewares/auth.ts
@@ -22,34 +22,33 @@ const authMiddleware =
           }),
         ),
       );
-    } else {
-      if (rules) {
-        const checks = rules.map(({ allow, roles, permissions }) => {
-          switch (allow) {
-            case "roles": {
-              return roles.some((role) => currentUser.roles.includes(role));
-            }
-            case "permissions": {
-              return permissions.some((permission) =>
-                currentUser.permissions.includes(permission),
-              );
-            }
-            default: {
-              return false;
-            }
+    } else if (rules) {
+      const checks = rules.map(({ allow, roles, permissions }) => {
+        switch (allow) {
+          case "roles": {
+            return roles.some((role) => currentUser.roles.includes(role));
           }
-        });
-
-        if (!checks.some((allowed) => allowed)) {
-          throw new ForbiddenError(
-            t("UNAUTHORIZED", {
-              ns: "error",
-            }),
-          );
+          case "permissions": {
+            return permissions.some((permission) =>
+              currentUser.permissions.includes(permission),
+            );
+          }
+          default: {
+            return false;
+          }
         }
+      });
+
+      if (!checks.some((allowed) => allowed)) {
+        throw new ForbiddenError(
+          t("UNAUTHORIZED", {
+            ns: "error",
+          }),
+        );
       }
-      next();
     }
+
+    next();
   };
 
 export default authMiddleware;
