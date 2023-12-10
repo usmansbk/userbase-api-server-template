843eea5675aa33f5d72da6a185a18f6cf7c7a348
usmansbk <usmansbk@gmail.com>
2023-12-10 01:29:14 +0100

    fix mutation

--- src/graphql/resolvers/User/Mutation/requestResetUserPassword.ts
+++ src/graphql/resolvers/User/Mutation/requestResetUserPassword.ts
@@ -10,63 +10,65 @@ import { PASSWORD_RESET_TOKEN_EXPIRES_IN } from "@/constants/limits";
 import { FORGOT_PASSWORD_TEMPLATE } from "@/constants/templates";
 
 export default {
-  async requestResetUserPassword(
-    _parent: unknown,
-    { email }: MutationRequestResetUserPasswordArgs,
-    context: AppContext,
-  ): Promise<MutationResponse> {
-    const { prismaClient, redisClient, jwtClient, emailClient, t } = context;
+  Mutation: {
+    async requestResetUserPassword(
+      _parent: unknown,
+      { email }: MutationRequestResetUserPasswordArgs,
+      context: AppContext,
+    ): Promise<MutationResponse> {
+      const { prismaClient, redisClient, jwtClient, emailClient, t } = context;
 
-    const cacheKey = `${PASSWORD_RESET_PREFIX}:${email}`;
-    const sentToken = await redisClient.get(cacheKey);
+      const cacheKey = `${PASSWORD_RESET_PREFIX}:${email}`;
+      const sentToken = await redisClient.get(cacheKey);
 
-    if (!sentToken) {
-      const user = await prismaClient.user.findFirst({
-        where: {
-          email,
-          status: {
-            in: [
-              UserStatus.Provisioned,
-              UserStatus.Active,
-              UserStatus.LockedOut,
-              UserStatus.PasswordExpired,
-              UserStatus.Recovery,
-            ],
-          },
-        },
-      });
-
-      if (user) {
-        const expiresIn = dayjs
-          .duration(...PASSWORD_RESET_TOKEN_EXPIRES_IN)
-          .asSeconds();
-
-        const token = jwtClient.signForAllClients(
-          { email },
-          {
-            expiresIn,
-          },
-        );
-
-        await redisClient.setex(cacheKey, expiresIn, token);
-
-        emailClient.send({
-          template: FORGOT_PASSWORD_TEMPLATE,
-          message: {
-            to: email,
-          },
-          locals: {
-            locale: user.language,
-            name: user.firstName,
-            link: token, // TODO: use universal link
+      if (!sentToken) {
+        const user = await prismaClient.user.findFirst({
+          where: {
+            email,
+            status: {
+              in: [
+                UserStatus.Provisioned,
+                UserStatus.Active,
+                UserStatus.LockedOut,
+                UserStatus.PasswordExpired,
+                UserStatus.Recovery,
+              ],
+            },
           },
         });
-      }
-    }
 
-    return {
-      success: true,
-      message: t("mutation.requestResetUserPassword.message"),
-    };
+        if (user) {
+          const expiresIn = dayjs
+            .duration(...PASSWORD_RESET_TOKEN_EXPIRES_IN)
+            .asSeconds();
+
+          const token = jwtClient.signForAllClients(
+            { email },
+            {
+              expiresIn,
+            },
+          );
+
+          await redisClient.setex(cacheKey, expiresIn, token);
+
+          emailClient.send({
+            template: FORGOT_PASSWORD_TEMPLATE,
+            message: {
+              to: email,
+            },
+            locals: {
+              locale: user.language,
+              name: user.firstName,
+              link: token, // TODO: use universal link
+            },
+          });
+        }
+      }
+
+      return {
+        success: true,
+        message: t("mutation.requestResetUserPassword.message"),
+      };
+    },
   },
 };
