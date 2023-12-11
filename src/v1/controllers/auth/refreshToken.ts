f9f1a450c5c40b29383592f1588a9466052235b6
usmansbk <usmansbk@gmail.com>
2023-12-11 08:59:35 +0100

    prevent refresh token brute-force

--- src/v1/controllers/auth/refreshToken.ts
+++ src/v1/controllers/auth/refreshToken.ts
@@ -1,10 +1,16 @@
 import { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
 import dayjs from "@/utils/dayjs";
-import { AUTH_PREFIX } from "@/constants/cachePrefixes";
+import { AUTH_PREFIX, LOGIN_ATTEMPT_PREFIX } from "@/constants/cachePrefixes";
 import AuthenticationError from "@/utils/errors/AuthenticationError";
-import { REFRESH_TOKEN_EXPIRES_IN } from "@/constants/limits";
+import {
+  BLOCK_IP_DURATION,
+  BRUTE_FORCE_THRESHOLD,
+  REFRESH_TOKEN_EXPIRES_IN,
+  RESET_LOGIN_ATTEMPTS_IN,
+} from "@/constants/limits";
 import type { NextFunction, Request, Response } from "express";
 import type { UserSessions } from "types";
+import { BLOCKED_IP_TEMPLATE } from "@/constants/templates";
 
 export default function refreshToken(
   req: Request,
@@ -12,7 +18,15 @@ export default function refreshToken(
   next: NextFunction,
 ) {
   (async () => {
-    const { prismaClient, redisClient, t, jwtClient, clientId } = req.context;
+    const {
+      prismaClient,
+      redisClient,
+      t,
+      jwtClient,
+      emailClient,
+      clientId,
+      ip,
+    } = req.context;
     const { access_token: expiredAccessToken, refresh_token: oldRefreshToken } =
       req.headers;
 
@@ -47,11 +61,61 @@ export default function refreshToken(
         throw new AuthenticationError(t("INVALID_AUTH_TOKEN", { ns: "error" }));
       }
 
+      const blockedIps = new Map(
+        Object.entries(user.blockedIps! as Record<string, string>),
+      );
+      const blockedIpAt = ip ? blockedIps.get(ip) : undefined;
+
+      const isBlocked =
+        blockedIpAt &&
+        dayjs().diff(blockedIpAt, "days") <= BLOCK_IP_DURATION[0];
+
+      if (isBlocked) {
+        throw new AuthenticationError(t("INVALID_AUTH_TOKEN", { ns: "error" }));
+      }
+
       const sessions = new Map(Object.entries(user.sessions as UserSessions));
 
       const oldSession = sessions.get(oldAzp!);
 
       if (!oldSession || oldSession.jti !== decodedRefreshToken.sub) {
+        const attemptsKey = `${LOGIN_ATTEMPT_PREFIX}:${ip}:${decodedRefreshToken.sub}`;
+        const attempts = await redisClient.get(attemptsKey);
+
+        const count = attempts ? Number.parseInt(attempts, 10) : 1;
+
+        if (count === BRUTE_FORCE_THRESHOLD) {
+          if (ip) {
+            blockedIps.set(ip, dayjs().toISOString());
+          }
+          await prismaClient.user.update({
+            where: {
+              id: user.id,
+            },
+            data: {
+              blockedIps: Object.fromEntries(blockedIps),
+            },
+          });
+
+          await redisClient.setex(
+            attemptsKey,
+            dayjs.duration(...RESET_LOGIN_ATTEMPTS_IN).asSeconds(),
+            count + 1,
+          );
+
+          if (user.isEmailVerified) {
+            emailClient.send({
+              template: BLOCKED_IP_TEMPLATE,
+              message: {
+                to: user.email,
+              },
+              locals: {
+                locale: user.language,
+                ip,
+              },
+            });
+          }
+        }
         throw new AuthenticationError(t("INVALID_AUTH_TOKEN", { ns: "error" }));
       }
 
