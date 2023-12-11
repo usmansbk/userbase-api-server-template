3428cc955a133b645420a747240dd4a741798214
usmansbk <usmansbk@gmail.com>
2023-12-11 18:05:05 +0100

    nit

--- src/constants/universalLinks.ts
+++ src/constants/universalLinks.ts
@@ -1,6 +1,7 @@
 const universalLink = process.env.UNIVERSAL_LINK;
 
 export default {
+  home: universalLink,
   leaveWaitlist: `${universalLink}/waitlist/leave/:token`,
   deleteAccount: `${universalLink}/user/delete/:token`,
   verifyEmail: `${universalLink}/verify-email/:token`,
