3260f2a03aa41109e79201d5369724c878918200
usmansbk <usmansbk@gmail.com>
2023-12-16 13:21:06 +0100

    restructure

---
+++ src/graphql/resolvers/admin/Query/userAvatar.ts
@@ -0,0 +1,28 @@
+import type { QueryUserAvatarArgs } from "types/graphql";
+import type { AppContext } from "types";
+import type { UserAvatar } from "@prisma/client";
+import QueryError from "@/utils/errors/QueryError";
+
+export default {
+  Query: {
+    async user(
+      _parent: unknown,
+      { id }: QueryUserAvatarArgs,
+      context: AppContext,
+    ): Promise<UserAvatar> {
+      const { prismaClient, t } = context;
+
+      const avatar = await prismaClient.userAvatar.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!avatar) {
+        throw new QueryError(t("query.userAvatar.errors.notFound"));
+      }
+
+      return avatar;
+    },
+  },
+};
