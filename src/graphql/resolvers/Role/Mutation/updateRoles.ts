f14da8f23f653975459a55fc852d666d3d5f62d7
usmansbk <usmansbk@gmail.com>
2023-12-17 20:54:18 +0100

    add validation

--- src/graphql/resolvers/Role/Mutation/updateRoles.ts
+++ src/graphql/resolvers/Role/Mutation/updateRoles.ts
@@ -3,8 +3,12 @@ import type { AppContext } from "types";
 import type { Role } from "@prisma/client";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import QueryError from "@/utils/errors/QueryError";
-import { ZodError } from "zod";
+import { ZodError, z } from "zod";
 import ValidationError from "@/utils/errors/ValidationError";
+import {
+  ROLE_DESCRIPTION_MAX_LENGTH,
+  ROLE_NAME_MAX_LENGTH,
+} from "@/constants/limits";
 
 export default {
   Mutation: {
@@ -16,6 +20,25 @@ export default {
       const { prismaClient, t } = context;
 
       try {
+        const inputSchema = z
+          .object({
+            name: z.string().max(
+              ROLE_NAME_MAX_LENGTH,
+              t("mutation.createRoles.errors.fields.name.max", {
+                count: ROLE_NAME_MAX_LENGTH,
+              }),
+            ),
+            description: z.string().max(
+              ROLE_DESCRIPTION_MAX_LENGTH,
+              t("mutation.createRoles.errors.fields.description.max", {
+                count: ROLE_DESCRIPTION_MAX_LENGTH,
+              }),
+            ),
+          })
+          .partial();
+
+        z.array(inputSchema).parse(inputs);
+
         return await prismaClient.$transaction(
           inputs.map(({ name, description, id }) =>
             prismaClient.role.update({
