9e9462043eed9e2bb832634549df32595991eb84
usmansbk <usmansbk@gmail.com>
2023-12-17 17:58:54 +0100

    handle error

--- src/graphql/resolvers/UserAvatar/Mutation/deleteUserAvatars.ts
+++ src/graphql/resolvers/UserAvatar/Mutation/deleteUserAvatars.ts
@@ -1,6 +1,8 @@
 import type { MutationDeleteUserAvatarsArgs } from "types/graphql";
 import type { AppContext } from "types";
 import type { UserAvatar } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import QueryError from "@/utils/errors/QueryError";
 
 export default {
   Mutation: {
@@ -9,17 +11,29 @@ export default {
       { inputs }: MutationDeleteUserAvatarsArgs,
       context: AppContext,
     ): Promise<UserAvatar[]> {
-      const { prismaClient } = context;
+      const { prismaClient, t } = context;
 
-      return await prismaClient.$transaction(
-        inputs.map(({ id }) =>
-          prismaClient.userAvatar.delete({
-            where: {
-              id,
-            },
-          }),
-        ),
-      );
+      try {
+        return await prismaClient.$transaction(
+          inputs.map(({ id }) =>
+            prismaClient.userAvatar.delete({
+              where: {
+                id,
+              },
+            }),
+          ),
+        );
+      } catch (e) {
+        if (e instanceof PrismaClientKnownRequestError) {
+          throw new QueryError(
+            t("mutation.deleteUserAvatars.errors.message", {
+              context: e.code as unknown,
+              count: inputs.length,
+            }),
+          );
+        }
+        throw e;
+      }
     },
   },
 };
