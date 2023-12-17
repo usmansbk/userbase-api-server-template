b051c112667c69491c281f816b16fcb4000d2672
usmansbk <usmansbk@gmail.com>
2023-12-17 17:30:52 +0100

    add i18next

--- src/graphql/resolvers/File/Mutation/deleteFiles.ts
+++ src/graphql/resolvers/File/Mutation/deleteFiles.ts
@@ -1,6 +1,8 @@
 import type { MutationDeleteFilesArgs } from "types/graphql";
 import type { AppContext } from "types";
 import type { File } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import QueryError from "@/utils/errors/QueryError";
 
 export default {
   Mutation: {
@@ -9,20 +11,32 @@ export default {
       { inputs }: MutationDeleteFilesArgs,
       context: AppContext,
     ): Promise<File[]> {
-      const { prismaClient } = context;
+      const { prismaClient, t } = context;
 
-      return await prismaClient.$transaction(
-        inputs.map(({ key, bucket }) =>
-          prismaClient.file.delete({
-            where: {
-              key_bucket: {
-                key,
-                bucket,
+      try {
+        return await prismaClient.$transaction(
+          inputs.map(({ key, bucket }) =>
+            prismaClient.file.delete({
+              where: {
+                key_bucket: {
+                  key,
+                  bucket,
+                },
               },
-            },
-          }),
-        ),
-      );
+            }),
+          ),
+        );
+      } catch (e) {
+        if (e instanceof PrismaClientKnownRequestError) {
+          throw new QueryError(
+            t("mutation.deleteFiles.errors.message", {
+              context: e.code as unknown,
+            }),
+            { originalError: e },
+          );
+        }
+        throw e;
+      }
     },
   },
 };
