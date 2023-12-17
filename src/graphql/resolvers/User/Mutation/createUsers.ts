0325ef68f548f0bd3432c1caa8abf8a23f82e4a2
usmansbk <usmansbk@gmail.com>
2023-12-17 21:06:40 +0100

    validate user input

--- src/graphql/resolvers/User/Mutation/createUsers.ts
+++ src/graphql/resolvers/User/Mutation/createUsers.ts
@@ -4,8 +4,14 @@ import type { MutationCreateUsersArgs } from "types/graphql";
 import type { AppContext } from "types";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import QueryError from "@/utils/errors/QueryError";
-import { ZodError } from "zod";
+import { ZodError, z } from "zod";
 import ValidationError from "@/utils/errors/ValidationError";
+import {
+  NAME_MAX_LENGTH,
+  NAME_MIN_LENGTH,
+  PASSWORD_MAX_LENGTH,
+  PASSWORD_MIN_LENGTH,
+} from "@/constants/limits";
 
 export default {
   Mutation: {
@@ -17,13 +23,108 @@ export default {
       const { prismaClient, t } = context;
 
       try {
+        const inputSchema = z.object({
+          email: z
+            .string({
+              required_error: t(
+                "mutation.registerWithEmail.errors.fields.email.required",
+              ),
+              invalid_type_error: t(
+                "mutation.registerWithEmail.errors.fields.email.invalidEmail",
+              ),
+            })
+            .trim()
+            .email(
+              t("mutation.registerWithEmail.errors.fields.email.invalidEmail"),
+            ),
+          firstName: z
+            .string()
+            .trim()
+            .min(
+              NAME_MIN_LENGTH,
+              t("mutation.registerWithEmail.errors.fields.name.min", {
+                count: NAME_MIN_LENGTH,
+              }),
+            )
+            .max(
+              NAME_MAX_LENGTH,
+              t("mutation.registerWithEmail.errors.fields.name.max", {
+                count: NAME_MAX_LENGTH,
+              }),
+            ),
+          lastName: z
+            .string()
+            .trim()
+            .min(
+              NAME_MIN_LENGTH,
+              t("mutation.registerWithEmail.errors.fields.name.min", {
+                count: NAME_MIN_LENGTH,
+              }),
+            )
+            .max(
+              NAME_MAX_LENGTH,
+              t("mutation.registerWithEmail.errors.fields.name.max", {
+                count: NAME_MAX_LENGTH,
+              }),
+            )
+            .optional(),
+          surname: z
+            .string()
+            .trim()
+            .min(
+              NAME_MIN_LENGTH,
+              t("mutation.registerWithEmail.errors.fields.name.min", {
+                count: NAME_MIN_LENGTH,
+              }),
+            )
+            .max(
+              NAME_MAX_LENGTH,
+              t("mutation.registerWithEmail.errors.fields.name.max", {
+                count: NAME_MAX_LENGTH,
+              }),
+            )
+            .optional(),
+          password: z
+            .string()
+            .trim()
+            .min(
+              PASSWORD_MIN_LENGTH,
+              t("mutation.registerWithEmail.errors.fields.password.min", {
+                count: PASSWORD_MIN_LENGTH,
+              }),
+            )
+            .max(
+              PASSWORD_MAX_LENGTH,
+              t("mutation.registerWithEmail.errors.fields.password.max", {
+                count: PASSWORD_MAX_LENGTH,
+              }),
+            )
+            .optional(),
+          status: z
+            .enum([
+              UserStatus.Active,
+              UserStatus.Deprovisioned,
+              UserStatus.LockedOut,
+              UserStatus.PasswordExpired,
+              UserStatus.Provisioned,
+              UserStatus.Recovery,
+              UserStatus.Staged,
+              UserStatus.Suspended,
+            ])
+            .default(UserStatus.Provisioned)
+            .optional(),
+          phoneNumber: z.string().optional(),
+          language: z.string().optional(),
+        });
+
+        const data = z.array(inputSchema).parse(inputs);
+
         return await prismaClient.$transaction(
-          inputs.map(({ password, status, ...data }) =>
+          data.map(({ password, ...data }) =>
             prismaClient.user.create({
               data: {
                 ...data,
                 password: password ?? nanoid(),
-                status: status ?? UserStatus.Provisioned,
               },
             }),
           ),
--- src/graphql/resolvers/User/Mutation/updateUsers.ts
+++ src/graphql/resolvers/User/Mutation/updateUsers.ts
@@ -1,10 +1,16 @@
-import type { User, UserStatus } from "@prisma/client";
+import { type User, UserStatus } from "@prisma/client";
 import type { MutationUpdateUsersArgs } from "types/graphql";
 import type { AppContext } from "types";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import QueryError from "@/utils/errors/QueryError";
-import { ZodError } from "zod";
+import { ZodError, z } from "zod";
 import ValidationError from "@/utils/errors/ValidationError";
+import {
+  NAME_MAX_LENGTH,
+  NAME_MIN_LENGTH,
+  PASSWORD_MAX_LENGTH,
+  PASSWORD_MIN_LENGTH,
+} from "@/constants/limits";
 
 export default {
   Mutation: {
@@ -16,6 +22,100 @@ export default {
       const { prismaClient, t } = context;
 
       try {
+        const inputSchema = z
+          .object({
+            email: z
+              .string({
+                required_error: t(
+                  "mutation.registerWithEmail.errors.fields.email.required",
+                ),
+                invalid_type_error: t(
+                  "mutation.registerWithEmail.errors.fields.email.invalidEmail",
+                ),
+              })
+              .trim()
+              .email(
+                t(
+                  "mutation.registerWithEmail.errors.fields.email.invalidEmail",
+                ),
+              ),
+            firstName: z
+              .string()
+              .trim()
+              .min(
+                NAME_MIN_LENGTH,
+                t("mutation.registerWithEmail.errors.fields.name.min", {
+                  count: NAME_MIN_LENGTH,
+                }),
+              )
+              .max(
+                NAME_MAX_LENGTH,
+                t("mutation.registerWithEmail.errors.fields.name.max", {
+                  count: NAME_MAX_LENGTH,
+                }),
+              ),
+            lastName: z
+              .string()
+              .trim()
+              .min(
+                NAME_MIN_LENGTH,
+                t("mutation.registerWithEmail.errors.fields.name.min", {
+                  count: NAME_MIN_LENGTH,
+                }),
+              )
+              .max(
+                NAME_MAX_LENGTH,
+                t("mutation.registerWithEmail.errors.fields.name.max", {
+                  count: NAME_MAX_LENGTH,
+                }),
+              ),
+            surname: z
+              .string()
+              .trim()
+              .min(
+                NAME_MIN_LENGTH,
+                t("mutation.registerWithEmail.errors.fields.name.min", {
+                  count: NAME_MIN_LENGTH,
+                }),
+              )
+              .max(
+                NAME_MAX_LENGTH,
+                t("mutation.registerWithEmail.errors.fields.name.max", {
+                  count: NAME_MAX_LENGTH,
+                }),
+              ),
+            password: z
+              .string()
+              .trim()
+              .min(
+                PASSWORD_MIN_LENGTH,
+                t("mutation.registerWithEmail.errors.fields.password.min", {
+                  count: PASSWORD_MIN_LENGTH,
+                }),
+              )
+              .max(
+                PASSWORD_MAX_LENGTH,
+                t("mutation.registerWithEmail.errors.fields.password.max", {
+                  count: PASSWORD_MAX_LENGTH,
+                }),
+              ),
+            status: z.enum([
+              UserStatus.Active,
+              UserStatus.Deprovisioned,
+              UserStatus.LockedOut,
+              UserStatus.PasswordExpired,
+              UserStatus.Provisioned,
+              UserStatus.Recovery,
+              UserStatus.Staged,
+              UserStatus.Suspended,
+            ]),
+            phoneNumber: z.string(),
+            language: z.string(),
+          })
+          .partial();
+
+        z.array(inputSchema).parse(inputs);
+
         return await prismaClient.$transaction(
           inputs.map(({ id, status, ...data }) =>
             prismaClient.user.update({
